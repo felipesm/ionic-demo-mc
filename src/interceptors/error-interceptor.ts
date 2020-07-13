cb03b7de0fc8ab9bb3fb1f2d03d446b7beb4c7fc
Felipe Maia <felipe.silwa@gmail.com>
2020-07-13 01:41:33 -0300

    Exibindo alerta para erros na aplicação

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,11 +3,12 @@ import { StorageService } from './../services/storageservice';
 import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage : StorageService) {
+    constructor(public storage : StorageService, public alert : AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,18 +27,70 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
                     case 403:
                         this.handle403();
                         break;
+                    case 404:
+                        this.handle404();
+                        break;
+                    default:
+                        this.handleDefaultError(errorObj);
+                        break;                    
                 }
 
                 return Observable.throw(errorObj);
             }) as any;
     }
+    
+    handle401() {
+        let alertControl = this.alert.create({
+            title: 'Falha na autenticação',
+            message: 'Email ou senha inválidos.',
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+
+        alertControl.present();
+    }
 
     handle403() {
         this.storage.setLocalUser(null);
     }
+
+    handle404() {
+        let alertControl = this.alert.create({
+            title: 'Página não encontrada',
+            message: 'Ihh que feio, favor tentar acessar mais tarde.',
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+
+        alertControl.present();
+    }
+
+    handleDefaultError(errorObj) {
+        let alertControl = this.alert.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+
+        alertControl.present();
+    }
+
 };
 
 export const ErrorInterceptorProvider = {
